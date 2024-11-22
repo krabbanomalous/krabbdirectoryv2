1c066c3872ac90c663e829592348faf28d940ad2
krabbanomalous <trentmoseley@outlook.com>
2024-11-21 21:44:56 -0600

    bg texture

---
+++ javas/blurbs.js
@@ -0,0 +1,19 @@
+const blurbs = ["Power to the people",
+    "Paradise is possible",
+    "It'll all work out",
+    "With you forever",
+    "Krabbanomalous was here",
+    "The edge of the universe",
+    "Welcome",
+    "The center of the universe",
+    "The air tastes of iron",
+    "You are needed",
+    "Life is worth it",
+    "It's worth being here",
+    "Heaven and Earth",
+    "You keep doing you",
+    "The future awaits",
+    "Can you see it?",
+];
+
+export default blurbs;
--- javas/htmlstuff.js
+++ javas/htmlstuff.js
@@ -1,4 +1,4 @@
-var blurbs = ["Power to the people...", "mexicanghëy", "krabbanomalous was here", "teehee", "ily", "Loading...", "look away if you don't like purple", "blerg seet", "some of these blurbs will only be understood by approximately one person in this world besides krabbanomlaous"]
-const the_blurb = document.getElementById("blurb")
+import blurbs from "./blurbs.js";
+const the_blurb = document.getElementById("blurb");
 
-the_blurb.innerHTML = blurbs[Math.floor(Math.random() * blurbs.length)]
+the_blurb.innerHTML = blurbs[Math.floor(Math.random() * blurbs.length)];
